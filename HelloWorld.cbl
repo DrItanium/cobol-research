b9554680085491179da0e71d5c5c0fcf528ffb78
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-02-07 20:49:02 -0800

    Added HelloWorld.cbl
    This is my first program written in COBOL

---
+++ HelloWorld.cbl
@@ -0,0 +1,8 @@
+      * Written by Joshua Scoggins
+      * OH GOD THE PAIN
+      * Better than fortran though :D
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-WORLD.
+       PROCEDURE DIVISION.
+         DISPLAY 'Hello, world'.
+         STOP RUN.
